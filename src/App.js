3bb8920c246184f8f524547c7b8be7105c283494
Biswajit Nayak <141207881+Biswajit81@users.noreply.github.com>
2023-11-22 16:04:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Loginpage from './Components/login/Loginpage';
+function App() {
+  return (
+    <div className="App">
+      <Loginpage />
+    </div>
+  );
+}
+
+export default App;
